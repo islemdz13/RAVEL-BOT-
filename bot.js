18ca1f7b6cf205547dacb1c6108f11306241715f
islemdz13 <45316190+islemdz13@users.noreply.github.com>
2018-11-25 16:36:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,39 @@ var prefix = "/";
 
 
 
-client.on("ready", () => {
-    console.log("انا مستعد")
-    client.user.setGame("By !...📛☠ ISLEM AP-13  ☠📛...! | /help");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame("By !...📛☠ ISLEM AP-13  ☠📛...! | /help")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 
 
-
+client.on('message', msg => {///حقوق بلو كودز
+  if (msg.content === 'هاي') {
+    msg.reply('هايات');
+  }
+});
 
 
 
