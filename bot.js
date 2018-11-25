16e69dac61726f3d6ff51766a3a13753ce499575
islemdz13 <45316190+islemdz13@users.noreply.github.com>
2018-11-25 16:28:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,29 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
 var prefix = "/";
 
-client.login(process.env.BOT_TOKEN);
+
 
 client.on("ready", () => {
     console.log("انا مستعد")
     client.user.setGame("By !...📛☠ ISLEM AP-13  ☠📛...! | /help");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
